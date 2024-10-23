74850a82b2d2831df9ebbccf375e37888d8735cf
nntdai <114330158+nntdai@users.noreply.github.com>
2024-10-23 23:37:34 +0700

    login x2

--- WebBookTour/src/main/java/com/example/WebBookTour/config/SecutityConfig.java
+++ WebBookTour/src/main/java/com/example/WebBookTour/config/SecutityConfig.java
@@ -4,16 +4,25 @@ package com.example.WebBookTour.config;
 import com.example.WebBookTour.repository.TaiKhoanAdminRepository;
 import com.example.WebBookTour.service.TaiKhoanAdminService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @Configuration
@@ -22,6 +31,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 @RequiredArgsConstructor
 public class SecutityConfig {
     private final TaiKhoanAdminRepository repository;
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -46,6 +56,17 @@ public class SecutityConfig {
 //                .build();
 //        return new InMemoryUserDetailsManager(admin, user);
         return new TaiKhoanAdminService(repository);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 
 }
