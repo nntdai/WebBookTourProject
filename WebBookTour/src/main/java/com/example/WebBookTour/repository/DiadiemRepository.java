12675497f227cd20c4b295a01271c5d5eb414f8d
nntdai <114330158+nntdai@users.noreply.github.com>
2024-11-24 15:56:20 +0700

    change

--- WebBookTour/src/main/java/com/example/WebBookTour/repository/DiadiemRepository.java
+++ WebBookTour/src/main/java/com/example/WebBookTour/repository/DiadiemRepository.java
@@ -2,6 +2,8 @@ package com.example.WebBookTour.repository;
 
 import com.example.WebBookTour.entity.Diadiem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DiadiemRepository extends JpaRepository<Diadiem, Integer> {
 }
